cd59a12f6b1e720c5144025faf39f0e90cd2c8c8
jaepoong <tjfwownd@naver.com>
2021-11-19 00:07:30 +0900

    unet

--- src/Unet/Unet.py
+++ src/Unet/Unet.py
@@ -1,3 +1,37 @@
 import os
 from parts import *
-oo()
+
+class UNet(nn.Module):
+    def __init__(self,n_channels,n_classes):
+        super(UNet,self).__isnit_s_()
+        self.n_channels=n_channels
+        self.n_classes=n_classes
+        
+        self.inc=nn.Sequential(Conv(n_channels,64),
+                               Conv(64,64))
+        self.down1=Downsampling(64,128)
+        self.down2=Downsampling(128,256)
+        self.down3=Downsampling(256,512)
+        self.down4=Downsampling(512,1024)
+        
+        self.up1=Upsampling(1024,512)
+        self.up2=Upsampling(512,256)
+        self.up3=Upsampling(256,128)
+        self.up4=Upsampling(128,64)
+        self.out=OutConv(64,n_classes)
+    
+    def forward(self,x):
+        x1=self.inc(x)
+        x2=self.down1(x1)
+        x3=self.down2(x2)
+        x4=self.down3(x3)
+        x5=self.down4(x4)
+        
+        x=self.up1(x5,x4)
+        x=self.up2(x,x3)
+        x=self.up3(x,x2)
+        x=self.up4(x,x1)
+        x=self.out(x)
+        return x
+        
+
